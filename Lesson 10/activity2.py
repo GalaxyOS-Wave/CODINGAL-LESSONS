f311afaa6399c3a7d71668ea9beb5afa0dc6981d
codingalcoder <dishantiscool236@gmail.com>
2026-01-08 19:04:02 +0530

    Lesson 10

---
+++ Lesson 10/activity2.py
@@ -0,0 +1,8 @@
+a = input("Please enter your own string")
+
+string2 = ('')
+for i in a:
+    string2 = i + string2
+
+print("\nThe Original string = ",a)
+print("\nThe Reversed string = ",string2)
---
+++ Lesson 10/activity3.py
@@ -0,0 +1,6 @@
+int1 = int(input("Enter a number : "))
+
+int2 = ('')
+for i in range(int1,1,-1):
+   print("The reversed order",i)
+    
